fa0cac005b8aaaee8f2b71c13ff302baed4ff25c
Sangbeom Heo <sangbeomheo@icloud.com>
2024-10-26 01:40:13 +0900

    [FE] feat: 회원가입

--- client/src/features/user/userSlice.js
+++ client/src/features/user/userSlice.js
@@ -1,35 +1,42 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
-import { showToastMessage } from "../common/uiSlice";
-import api from "../../utils/api";
-import { initialCart } from "../cart/cartSlice";
+import { showToastMessage } from '../common/uiSlice'
+import api from '../../utils/api'
+import { initialCart } from '../cart/cartSlice'
 
 export const loginWithEmail = createAsyncThunk(
-  "user/loginWithEmail",
+  'user/loginWithEmail',
   async ({ email, password }, { rejectWithValue }) => {}
-);
+)
 
 export const loginWithGoogle = createAsyncThunk(
-  "user/loginWithGoogle",
+  'user/loginWithGoogle',
   async (token, { rejectWithValue }) => {}
-);
+)
 
-export const logout = () => (dispatch) => {};
+export const logout = () => dispatch => {}
 export const registerUser = createAsyncThunk(
-  "user/registerUser",
-  async (
-    { email, name, password, navigate },
-    { dispatch, rejectWithValue }
-  ) => {}
-);
+  'user/registerUser',
+  async ({ email, name, password, navigate }, { dispatch, rejectWithValue }) => {
+    try {
+      const response = await api.post('/user', { email, name, password })
+      dispatch(showToastMessage({ message: '회원가입 성공', status: 'success' }))
+      navigate('/login')
+      return response.data.data
+    } catch (error) {
+      dispatch(showToastMessage({ message: '회원가입 실패', status: 'error' }))
+      return rejectWithValue(error.message)
+    }
+  }
+)
 
 export const loginWithToken = createAsyncThunk(
-  "user/loginWithToken",
+  'user/loginWithToken',
   async (_, { rejectWithValue }) => {}
-);
+)
 
 const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState: {
     user: null,
     loading: false,
@@ -38,12 +45,25 @@ const userSlice = createSlice({
     success: false,
   },
   reducers: {
-    clearErrors: (state) => {
-      state.loginError = null;
-      state.registrationError = null;
+    clearErrors: state => {
+      state.loginError = null
+      state.registrationError = null
     },
   },
-  extraReducers: (builder) => {},
-});
-export const { clearErrors } = userSlice.actions;
-export default userSlice.reducer;
+  extraReducers: builder => {
+    builder
+      .addCase(registerUser.pending, state => {
+        state.loading = true
+      })
+      .addCase(registerUser.fulfilled, state => {
+        state.loading = false
+        state.registrationError = null
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.loading = false
+        state.registrationError = action.payload
+      })
+  },
+})
+export const { clearErrors } = userSlice.actions
+export default userSlice.reducer
