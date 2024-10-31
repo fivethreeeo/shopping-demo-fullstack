0e4bbfd8f241510efa25434d8048c3c472ff38db
Sangbeom Heo <sangbeomheo@icloud.com>
2024-11-01 01:11:56 +0900

    [BE] feat: 상품검색, 페이지네이션

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcryptjs')
 const Product = require('../models/Product')
 require('dotenv').config()
 
+const PAGE_SIZE = 5
+
 const productController = {}
 
 productController.createProduct = async (req, res) => {
@@ -29,8 +31,20 @@ productController.createProduct = async (req, res) => {
 
 productController.getProducts = async (req, res) => {
   try {
-    const products = await Product.find({})
-    res.status(200).json({ status: 'success', products })
+    const { page, name } = req.query
+    const condition = name ? { name: { $regex: name, $options: 'i' } } : {}
+    let query = Product.find(condition)
+    let response = { status: 'success' }
+    if (page) {
+      const prevPage = page - 1
+      query.skip(prevPage * PAGE_SIZE).limit(PAGE_SIZE)
+      const totalItemCount = await Product.find(condition).countDocuments()
+      const totalPageNum = Math.ceil(totalItemCount / PAGE_SIZE)
+      response.totalPageNum = totalPageNum
+    }
+    const products = await query.exec()
+    response.products = products
+    res.status(200).json(response)
   } catch (error) {
     res.status(400).json({ status: 'fail', message: error.message })
   }
